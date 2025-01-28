3ab33ecb0f24113f1786df6db0bb9a927d62423a
Vibhu <vbhargav13@gmail.com>
2025-01-28 14:50:34 +0530

    Extra lines added in ver 2.2

--- LinearSearch.py
+++ LinearSearch.py
@@ -6,3 +6,4 @@ file.write("Today is Thursday.")  # Write text to the file
 file.close()  # Always close the file after use
 "vnvbmn,"
 Extra lines Added in python File.
+Extra lines Again Added in python File.
