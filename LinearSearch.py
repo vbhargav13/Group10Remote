657b107b7996fdc48eff9160c8b4fe7a16bea9ea
Vibhu <vbhargav13@gmail.com>
2025-01-28 14:40:32 +0530

    Python code vs 1.1

--- LinearSearch.py
+++ LinearSearch.py
@@ -5,3 +5,4 @@ file = open("example.txt", "a")
 file.write("Today is Thursday.")  # Write text to the file
 file.close()  # Always close the file after use
 "vnvbmn,"
+Extra lines Added in python File.
