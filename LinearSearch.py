97e28c0883c5fe5cc6b2e6f7ede04e4925645113
Vibhu <vbhargav13@gmail.com>
2025-01-28 11:50:50 +0530

    Linear Search ver1.2

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,4 +1,7 @@
 # Open a file in write mode ('w') and write data
 file = open("example.txt", "a")
+
+"gjghjgkj
 file.write("Today is Thursday.")  # Write text to the file
 file.close()  # Always close the file after use
+"vnvbmn,"
